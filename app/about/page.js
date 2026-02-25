2bf28b950107e3b385ad4cf100ff4091965e8a51
alaughlin1973 <60031298+alaughlin1973@users.noreply.github.com>
2026-02-25 22:24:56 +0000

    Add files via upload

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main className="page">
+      <h1>About EF Aesthetics</h1>
+      <p>
+        EF Aesthetics is a luxury beauty and aesthetic studio 
+        located in Benalmádena, specialising in Laser Hair Removal,
+        Hair Styling and Professional Makeup services.
+      </p>
+    </main>
+  );
+}
+
