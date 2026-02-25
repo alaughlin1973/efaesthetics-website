a4168802f37ad5778b68d8f420476df076357b95
alaughlin1973 <60031298+alaughlin1973@users.noreply.github.com>
2026-02-25 22:06:20 +0000

    Add files via upload

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
 
       <Link href="/">
         <Image
-          src="/logo.png"
+          src="/logo1.png"
           alt="EF Aesthetics"
           width={120}
           height={60}
